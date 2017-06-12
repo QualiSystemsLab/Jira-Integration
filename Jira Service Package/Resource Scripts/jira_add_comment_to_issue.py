f607f91ab3e70bb7dc3ac86ba709a6e37d080f88
ericrqs <eric.r@quali.com>
2017-06-12 19:03:56 -0400

    remove dependency on python requests package

--- Jira Service Package/Resource Scripts/jira_add_comment_to_issue.py
+++ Jira Service Package/Resource Scripts/jira_add_comment_to_issue.py
@@ -1,8 +1,12 @@
 import cloudshell.helpers.scripts.cloudshell_scripts_helpers as helpers
-
+import re
 import os
+import base64
 import json
-import requests
+# import requests
+from urllib2 import Request
+from urllib2 import urlopen
+from urllib import quote
 
 api = helpers.get_api_session()
 rc = json.loads(os.environ['RESOURCECONTEXT'])
@@ -15,6 +19,11 @@ issueid = os.environ['ISSUE_ID']
 comment = os.environ['COMMENT']
 fields_json = os.environ['ADDITIONAL_FIELDS_JSON']
 
+def bytes23(s):
+    if isinstance(s, unicode):
+        return s.encode('utf-8', 'replace')
+    else:
+        return s or b''
 
 def log(s):
     try:
@@ -23,23 +32,70 @@ def log(s):
     except:
         pass
 
+def _request(method, path, data=None, headers=None, hide_result=False, **kwargs):
+    if not headers:
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + base64.b64encode('%s:%s' % (user, password)),
+        }
+    # if self._token:
+        # headers['Authorization'] = 'Basic ' + self._token
 
-def req(method, urlsuffix, body='', *args, **kwargs):
-    url = '%s/%s' % (urlbase, urlsuffix)
-    log('Request %s %s %s' % (method, url, body))
-    r = requests.request(method, url,
-                         auth=(user, password),
-                         headers={
-                             'Content-Type': 'application/json',
-                             'Accept': 'application/json',
-                         },
-                         data=body if body else None)
-    log('Result: %d: %s' % (r.status_code, r.text))
-    if r.status_code >= 400:
-        raise Exception('Error %d: %s' % (r.status_code, r.text))
-    return r.text
+    if path.startswith('/'):
+        path = path[1:]
 
-print req('post', '/rest/api/2/issue/%s/comment' % issueid, '''{
+    url = '%s/%s' % (urlbase, path)
+
+    if isinstance(url, unicode):
+        url = url.encode('ascii')
+    headers = dict((k.encode('ascii') if isinstance(k, unicode) else k,
+                    v.encode('ascii') if isinstance(v, unicode) else v)
+                   for k, v in headers.items())
+
+    # pdata = data
+    # pdata = re.sub(r':[^@]*@', ':(password hidden)@', pdata)
+    # pdata = re.sub(r'"Password":\s*"[^"]*"', '"Password": "(password hidden)"', pdata)
+    # pheaders = dict(headers)
+    # if 'Authorization' in pheaders:
+    #     pheaders['Authorization'] = '(token hidden)'
+
+    # log('Request %d: %s %s headers=%s data=<<<%s>>>' % (counter, method, url, pheaders, pdata))
+
+    request = Request(url, bytes23(data), headers)
+    request.get_method = lambda: method.upper()
+    response = urlopen(request)
+    body = response.read()
+    code = response.getcode()
+    response.close()
+
+    if code >= 400:
+        raise Exception('Error: %d: %s' % (code, body))
+    return code, body
+
+# def req(method, urlsuffix, body='', *args, **kwargs):
+#     url = '%s/%s' % (urlbase, urlsuffix)
+#     log('Request %s %s %s' % (method, url, body))
+#     r = requests.request(method, url,
+#                          auth=(user, password),
+#                          headers={
+#                              'Content-Type': 'application/json',
+#                              'Accept': 'application/json',
+#                          },
+#                          data=body if body else None)
+#     log('Result: %d: %s' % (r.status_code, r.text))
+#     if r.status_code >= 400:
+#         raise Exception('Error %d: %s' % (r.status_code, r.text))
+#     return r.text
+
+_, rs = _request('post', '/rest/api/2/issue/%s/comment' % issueid,
+              data='''{
     "body": "%s"
     %s
 }''' % (comment, ','+fields_json if fields_json else ''))
+
+print rs
+# print req('post', '/rest/api/2/issue/%s/comment' % issueid, '''{
+#     "body": "%s"
+#     %s
+# }''' % (comment, ','+fields_json if fields_json else ''))
--- Jira Service Package/Resource Scripts/jira_create_issue.py
+++ Jira Service Package/Resource Scripts/jira_create_issue.py
@@ -1,8 +1,12 @@
 import cloudshell.helpers.scripts.cloudshell_scripts_helpers as helpers
-
+import re
 import os
 import json
-import requests
+import base64
+# import requests
+from urllib2 import Request
+from urllib2 import urlopen
+from urllib import quote
 
 api = helpers.get_api_session()
 rc = json.loads(os.environ['RESOURCECONTEXT'])
@@ -17,6 +21,11 @@ title = os.environ['TITLE']
 descr = os.environ['DESCRIPTION']
 fields_json = os.environ['ADDITIONAL_FIELDS_JSON']
 
+def bytes23(s):
+    if isinstance(s, unicode):
+        return s.encode('utf-8', 'replace')
+    else:
+        return s or b''
 
 def log(s):
     try:
@@ -25,25 +34,66 @@ def log(s):
     except:
         pass
 
+def _request(method, path, data=None, headers=None, hide_result=False, **kwargs):
+    if not headers:
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + base64.b64encode('%s:%s' % (user, password))
+        }
+    # if self._token:
+        # headers['Authorization'] = 'Basic ' + self._token
 
-def req(method, urlsuffix, body='', *args, **kwargs):
-    url = '%s/%s' % (urlbase, urlsuffix)
-    log('Request %s %s %s' % (method, url, body))
-    r = requests.request(method, url,
-                         auth=(user, password),
-                         headers={
-                             'Content-Type': 'application/json',
-                             'Accept': 'application/json',
-                         },
-                         data=body if body else None)
-    log('Result: %d: %s' % (r.status_code, r.text))
-    if r.status_code >= 400:
-        raise Exception('Error %d: %s' % (r.status_code, r.text))
-    return r.text
+    if path.startswith('/'):
+        path = path[1:]
+
+    url = '%s/%s' % (urlbase, path)
+
+    if isinstance(url, unicode):
+        url = url.encode('ascii')
+    headers = dict((k.encode('ascii') if isinstance(k, unicode) else k,
+                    v.encode('ascii') if isinstance(v, unicode) else v)
+                   for k, v in headers.items())
+
+    # pdata = data
+    # pdata = re.sub(r':[^@]*@', ':(password hidden)@', pdata)
+    # pdata = re.sub(r'"Password":\s*"[^"]*"', '"Password": "(password hidden)"', pdata)
+    # pheaders = dict(headers)
+    # if 'Authorization' in pheaders:
+    #     pheaders['Authorization'] = '(token hidden)'
+
+    # log('Request %d: %s %s headers=%s data=<<<%s>>>' % (counter, method, url, pheaders, pdata))
+
+    request = Request(url, bytes23(data), headers)
+    request.get_method = lambda: method.upper()
+    response = urlopen(request)
+    body = response.read()
+    code = response.getcode()
+    response.close()
+
+    if code >= 400:
+        raise Exception('Error: %d: %s' % (code, body))
+    return code, body
+
+# def req(method, urlsuffix, body='', *args, **kwargs):
+#     url = '%s/%s' % (urlbase, urlsuffix)
+#     log('Request %s %s %s' % (method, url, body))
+#     r = requests.request(method, url,
+#                          auth=(user, password),
+#                          headers={
+#                              'Content-Type': 'application/json',
+#                              'Accept': 'application/json',
+#                          },
+#                          data=body if body else None)
+#     log('Result: %d: %s' % (r.status_code, r.text))
+#     if r.status_code >= 400:
+#         raise Exception('Error %d: %s' % (r.status_code, r.text))
+#     return r.text
 
 defaultprojid = ''
 projid = ''
-projs = json.loads(req('get', '/rest/api/2/project'))
+_, bls = _request('get', '/rest/api/2/project')
+projs = json.loads(bls)
 for proj in projs:
     if not defaultprojid:
         defaultprojid = proj['id']
@@ -61,7 +111,8 @@ else:
 
 
 issuetypeid = ''
-its = json.loads(req('get', '/rest/api/2/issuetype'))
+_, nn = _request('get', '/rest/api/2/issuetype')
+its = json.loads(nn)
 for it in its:
     if it['name'] == issuetypename:
         issuetypeid = it['id']
@@ -70,7 +121,7 @@ for it in its:
 if not issuetypeid:
     raise Exception('Issue type "%s" not found. Valid types are %s' % (issuetypename, ', '.join([it['name'] for it in its])))
 
-print req('post', '/rest/api/2/issue', '''{
+_, rslt = _request('post', '/rest/api/2/issue', data='''{
     "fields": {
         "project": {
             "id": %s
@@ -83,3 +134,5 @@ print req('post', '/rest/api/2/issue', '''{
         }
     }
 }''' % (projid, fields_json, title, descr, issuetypeid))
+
+print rslt
