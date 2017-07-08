acc3026e486e6780f15927aabe3d048095358767
ericrqs <eric.r@quali.com>
2017-07-07 17:33:26 -0700

    switch to abstract Cisco Nexus

--- Jira Service Package/Resource Scripts/health_check.py
+++ Jira Service Package/Resource Scripts/health_check.py
@@ -3,7 +3,7 @@ import cloudshell.helpers.scripts.cloudshell_scripts_helpers as helpers
 api = helpers.get_api_session()
 fullname = helpers.get_resource_context_details().fullname
 
-if 'Failing' in fullname:
+if 'DUT 2' in fullname:
     rv = 'Health check on resource %s failed: Device not responding' % fullname
     api.SetResourceLiveStatus(fullname, 'Error', rv)
 else:
